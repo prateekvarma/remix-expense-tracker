d3dc9e4609cebcc7035e1eab4a194e9b9c51b5cb
prateekvarma <prtk.varma@gmail.com>
2023-04-08 15:26:45 -0600

    Added styles for auth and expenses

--- app/routes/expenses.jsx
+++ app/routes/expenses.jsx
@@ -1,10 +1,15 @@
-import { Outlet } from "@remix-run/react";
+import { Outlet } from '@remix-run/react';
+import expensesStyles from '~/styles/expenses.css';
 
 export default function ExpensesLayout() {
-    return (
-        <main>
-            <p>Shared element</p>
-            <Outlet />
-        </main>
-    )
+  return (
+    <main>
+      <p>Shared element</p>
+      <Outlet />
+    </main>
+  );
+}
+
+export function links() {
+  return [{ rel: 'stylesheet', href: expensesStyles }];
 }
