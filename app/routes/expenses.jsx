6d04a6a7b3dc90dc44473a3d8bf5e8c646570860
prateekvarma <prtk.varma@gmail.com>
2023-04-10 15:21:54 -0600

    Added some shared layout in expenses

--- app/routes/expenses.jsx
+++ app/routes/expenses.jsx
@@ -1,12 +1,30 @@
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'first title',
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: 'e2',
+    title: 'second title',
+    amount: 16.99,
+    date: new Date().toISOString(),
+  },
+];
+
 import { Outlet } from '@remix-run/react';
+import ExpensesList from '~/components/expenses/ExpensesList';
 import expensesStyles from '~/styles/expenses.css';
 
 export default function ExpensesLayout() {
   return (
-    <main>
-      <p>Shared element</p>
+    <>
       <Outlet />
-    </main>
+      <main>
+        <ExpensesList expenses={DUMMY_EXPENSES} />
+      </main>
+    </>
   );
 }
 
