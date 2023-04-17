bce8474258d345208ffd0d87a76c02c012b293e0
prateekvarma <prtk.varma@gmail.com>
2023-04-17 15:54:38 -0600

    Displaying a separate header for 'marketing' nested page

--- app/routes/_marketing.jsx
+++ app/routes/_marketing.jsx
@@ -2,7 +2,10 @@ import { Outlet } from '@remix-run/react';
 import marketingStyles from '~/styles/marketing.css';
 
 export default function MarketingLayout() {
-  return <Outlet />;
+  return;
+  <>
+    <Outlet />
+  </>;
 }
 
 export function links() {
