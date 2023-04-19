d2c6df00b3576322266c340a0d7b92c407670b30
prateekvarma <prtk.varma@gmail.com>
2023-04-19 15:50:46 -0600

    /expenses/raw route returns raw data using loader function

--- app/routes/expenses.raw.jsx
+++ app/routes/expenses.raw.jsx
@@ -1,5 +1,18 @@
-export default function ExpensesRawPage() {
-    return (
-        <h1>Expenses Raw Page</h1>
-    )
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'first title',
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: 'e2',
+    title: 'second title',
+    amount: 16.99,
+    date: new Date().toISOString(),
+  },
+];
+
+export function loader() {
+  return DUMMY_EXPENSES;
 }
