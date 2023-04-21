32e8dd2443df5ca9e63217b199b2e1c6bd88d431
prateekvarma <prtk.varma@gmail.com>
2023-04-21 13:32:33 -0600

    Added buttons on expenses page

--- app/routes/_app.expenses.jsx
+++ app/routes/_app.expenses.jsx
@@ -13,7 +13,8 @@ const DUMMY_EXPENSES = [
   },
 ];
 
-import { Outlet } from '@remix-run/react';
+import { Link, Outlet } from '@remix-run/react';
+import { FaDownload, FaPlus } from 'react-icons/fa';
 import ExpensesList from '~/components/expenses/ExpensesList';
 import expensesStyles from '~/styles/expenses.css';
 // tbd: create a new layout file called _app.tsx and put all the expenses files in it, so they can share the mainHeader
@@ -22,6 +23,17 @@ export default function ExpensesLayout() {
     <>
       <Outlet />
       <main>
+        <section id='expenses-actions'>
+          {/* below, adding a relative link */}
+          <Link to='add'>
+            <FaPlus />
+            <span>Add Expense</span>
+          </Link>
+          <a href='/expenses/raw'>
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
         <ExpensesList expenses={DUMMY_EXPENSES} />
       </main>
     </>
