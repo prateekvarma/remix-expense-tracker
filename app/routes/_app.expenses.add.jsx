9c32f7612651de2d36d54727b39ef16a94eb07c9
prateekvarma <prtk.varma@gmail.com>
2023-04-29 18:04:56 -0600

    Applying server side validation for form data

--- app/routes/_app.expenses.add.jsx
+++ app/routes/_app.expenses.add.jsx
@@ -3,6 +3,7 @@ import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '~/components/util/Modal';
 import { addExpense } from '../data/expenses.server';
 import { redirect } from '@remix-run/node';
+import { validateExpenseInput } from '../data/validation.server';
 
 export default function AddExpensesPage() {
   const navigate = useNavigate();
@@ -23,6 +24,13 @@ export async function action({ request }) {
   const formData = await request.formData();
   // formData.get('title') //to get individual entries, or:
   const expenseData = Object.fromEntries(formData); //made an object from the form data
+  //validation
+  try {
+    validateExpenseInput(expenseData); //this will throw en error, if caught any validation errors
+  } catch (error) {
+    //validation failed
+    return error;
+  }
   await addExpense(expenseData);
-  return redirect('/expenses')
+  return redirect('/expenses');
 }
