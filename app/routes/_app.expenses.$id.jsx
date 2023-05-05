ce111118b77da174e3d3f2936190d6cc543b90e7
prateekvarma <prtk.varma@gmail.com>
2023-05-05 15:09:03 -0600

    Created action function to update expense

--- app/routes/_app.expenses.$id.jsx
+++ app/routes/_app.expenses.$id.jsx
@@ -2,6 +2,9 @@
 import { useNavigate } from '@remix-run/react';
 import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '~/components/util/Modal';
+import { updateExpense } from '../data/expenses.server';
+import { redirect } from '@remix-run/node';
+import { validateExpenseInput } from '../data/validation.server';
 // import { getExpense } from '../data/expenses.server';
 
 export default function UpdateExpensesPage() {
@@ -19,7 +22,23 @@ export default function UpdateExpensesPage() {
 }
 
 // export async function loader({ params }) {
+// old loader which extracts and returns a single expense
 //   const expenseId = params.id;
 //   const expense = await getExpense(expenseId);
 //   return expense;
 // }
+
+export async function action({ params, request }) {
+  const expenseId = params.id;
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await updateExpense(expenseId, expenseData);
+  return redirect('/expenses');
+}
