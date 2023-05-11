3a893753b9b3d1d5f80719a0a97794543eb257a4
prateekvarma <prtk.varma@gmail.com>
2023-05-11 17:05:20 -0600

    Made a delete request using useFetcher()

--- app/components/expenses/ExpenseListItem.jsx
+++ app/components/expenses/ExpenseListItem.jsx
@@ -1,21 +1,22 @@
-import { Form, Link } from "@remix-run/react";
+import { Form, Link, useFetcher } from '@remix-run/react';
 
 function ExpenseListItem({ id, title, amount }) {
+  const fetcher = useFetcher();
   function deleteExpenseItemHandler() {
-    // tbd
+    fetcher.submit(null, { method: 'DELETE', action: `/expenses/${id}` });
   }
 
   return (
-    <article className="expense-item">
+    <article className='expense-item'>
       <div>
-        <h2 className="expense-title">{title}</h2>
-        <p className="expense-amount">${amount.toFixed(2)}</p>
+        <h2 className='expense-title'>{title}</h2>
+        <p className='expense-amount'>${amount.toFixed(2)}</p>
       </div>
-      <menu className="expense-actions">
-        {/* <button onClick={deleteExpenseItemHandler}>Delete</button> */}
-        <Form method='delete' action={`/expenses/${id}`}>
+      <menu className='expense-actions'>
+        <button onClick={deleteExpenseItemHandler}>Delete</button>
+        {/* <Form method='delete' action={`/expenses/${id}`}>
           <button>Delete</button>
-        </Form>
+        </Form> */}
         <Link to={id}>Edit</Link>
       </menu>
     </article>
