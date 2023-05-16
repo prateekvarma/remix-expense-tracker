3fa028f05013e35eab65e93c3bed2d15c1c18dc4
prateekvarma <prtk.varma@gmail.com>
2023-05-15 22:09:56 -0600

    Using credentials from auth form (incomplete)

--- app/routes/_marketing.auth.jsx
+++ app/routes/_marketing.auth.jsx
@@ -5,6 +5,12 @@ export default function AuthPage() {
   return <AuthForm />;
 }
 
+export async function action({request}) {
+  //triggers then form is submitted
+  const formData = await request.formData();
+  const credentials = Object.fromEntries(formData);
+}
+
 export function links() {
   return [{ rel: 'stylesheet', href: authStyles }];
 }
