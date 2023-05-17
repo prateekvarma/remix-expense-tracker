cebedb9888b89ea1bfe95a8050c45396ff3938d7
prateekvarma <prtk.varma@gmail.com>
2023-05-17 16:23:27 -0600

    Used ErrorBoundary to catch errors and display custom messages

--- app/root.jsx
+++ app/root.jsx
@@ -1,31 +1,61 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from '@remix-run/react';
 
 import sharedStyles from '../app/styles/shared.css';
 
-export default function App() {
+function Document({ title, children }) {
   return (
     <html lang='en'>
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <title>{title}</title>
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  )
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError()
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <Document title={error.status}>
+      <main>
+        <div>
+          <p>{error.status}: {error.statusText}</p>
+          <p>{error.data?.message || 'Something went wrong'}</p>
+          <p>Back to <Link to='/'>safety</Link>.</p>
+        </div>
+      </main>
+    </Document>
+    );
+  }
 }
 
 export function links() {
