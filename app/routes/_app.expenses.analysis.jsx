937c6d486fb9dcd084fab7487007a862cf05329f
prateekvarma <prtk.varma@gmail.com>
2023-05-23 16:17:58 -0600

    Defined a loader function for analysis

--- app/routes/_app.expenses.analysis.jsx
+++ app/routes/_app.expenses.analysis.jsx
@@ -1,26 +1,29 @@
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'first title',
-    amount: 12.99,
-    date: new Date().toISOString(),
-  },
-  {
-    id: 'e2',
-    title: 'second title',
-    amount: 16.99,
-    date: new Date().toISOString(),
-  },
-];
-
 import ExpenseStatistics from '~/components/expenses/ExpenseStatistics';
 import Chart from '~/components/expenses/Chart';
+import { getExpenses } from '../data/expenses.server';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 
 export default function ExpensesAnalysisPage() {
+  const expenses = useLoaderData();
+
+
   return (
     <main>
       <Chart expenses={DUMMY_EXPENSES} />
       <ExpenseStatistics expenses={DUMMY_EXPENSES} />
     </main>
   );
+}
+
+export async function loader() {
+  const expenses = await getExpenses();
+  if (!expenses || expenses.length === 0) {
+    throw json(
+      { message: 'Could not load data from expenses analysis!' },
+      { status: 404, statusText: 'Expenses not found!' }
+    );
+  }
+
+  return expenses; // means return json(expenses) as remix does this for us
 }
