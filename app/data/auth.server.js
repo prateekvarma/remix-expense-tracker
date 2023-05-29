ef4a29be9922c861693da10ff73437a8359f0104
prateekvarma <prtk.varma@gmail.com>
2023-05-29 14:00:53 -0600

    Login function backend, with error handling

--- app/data/auth.server.js
+++ app/data/auth.server.js
@@ -1,17 +1,38 @@
-import { hash } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
 
 import { prisma } from './database.server';
 
 export async function signup({ email, password }) {
   const existingUser = await prisma.user.findFirst({ where: { email } });
-
+  
   if (existingUser) {
     const error = new Error('A user with this email already exists'); //works
     error.status = 422; //does not work
     throw error;
   }
-
+  
   const passwordHash = await hash(password, 12);
-
+  
   await prisma.user.create({ data: { email: email, password: passwordHash } });
+}
+
+export async function login({ email, password }) {
+  const existingUser = await prisma.user.findFirst({ where: { email } });
+
+  if(!existingUser) {
+    const error = new Error('Could not find a user with this email'); 
+    error.status = 401; 
+    throw error;
+  }
+
+  const isPasswordCorrect = await compare(password, existingUser.password)
+
+  if(!isPasswordCorrect) {
+    const error = new Error('Incorrect password'); 
+    error.status = 401; 
+    throw error;
+  }
+
+  // auth success
+  
 }
